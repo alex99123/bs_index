31c0157072684165b70aec416a19a8cac10c89b0
honghui.cai@bosicloud.com-at-917958955567 <honghui.cai@bosicloud.com>
2019-04-17 16:34:03 +0800

    beanstalk

---
+++ application.py
@@ -0,0 +1,48 @@
+from flask import Flask,render_template
+
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+@application.route('/index.html')
+def index1():
+    return render_template('index.html')
+
+@application.route('/devOps.html')
+def devOps():
+    return render_template('devOps.html')
+
+@application.route('/bigDataAndAI.html')
+def bigDataAndAI():
+    return render_template('bigDataAndAI.html')
+
+@application.route('/demoList.html')
+def demoList():
+    return render_template('demoList.html')
+
+@application.route('/DevOpsList.html')
+def DevOpsList():
+    return render_template('DevOpsList.html')
+
+@application.route('/galanz.html')
+def galanz():
+    return render_template('galanz.html')
+
+@application.route('/logAndScheme.html')
+def logAndScheme():
+    return render_template('logAndScheme.html')
+
+@application.route('/newList.html')
+def newList():
+    return render_template('newList.html')
+
+@application.route('/tsl.html')
+def tsl():
+    return render_template('tsl.html')
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
